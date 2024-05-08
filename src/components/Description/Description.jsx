87c96958857896cd2f763cf4e8ccd0cfbf7bf988
TValeriiaM <149121749+TValeriiaM@users.noreply.github.com>
2024-05-09 02:17:54 +0300

    hw02_styles

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -2,9 +2,9 @@ import css from "./Description.module.css"
 
 export default function Description() {
     return <>
-        <h1>Sip Happens Café
+        <h1 className={css.header}>Sip Happens Café
 </h1>
-        <p>Please leave your feedback about our service by selecting one of the options below.
+        <p className={css.note}>Please leave your feedback about our service by selecting one of the options below.
 </p>
     </>
 }
